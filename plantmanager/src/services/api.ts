670e8fbab3a80799762b52b80e45ac4f27dea984
Pedro <phbirais@gmail.com>
2021-04-25 07:54:58 -0300

    Back configs

--- plantmanager/src/services/api.ts
+++ plantmanager/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/phBirais/Planty_',
-    //baseURL: 'http://192.168.18.19:3333',
+    //baseURL: 'https://my-json-server.typicode.com/phBirais/Planty_',
+    baseURL: 'http://192.168.18.19:3333',
 });
 
 
