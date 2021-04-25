31e7ed10b1374d14f6a9e4f44ce74ff265696376
Pedro <phbirais@gmail.com>
2021-04-25 07:49:31 -0300

    urk json server

--- plantmanager/src/services/api.ts
+++ plantmanager/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/https://github.com/phBirais/Planty_',
+    baseURL: 'https://my-json-server.typicode.com/phBirais/Planty_',
     //baseURL: 'http://192.168.18.19:3333',
 });
 
